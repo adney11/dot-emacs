e6274a269d6524b4716962b65e821c26e9518b7d
Adney <adney11@gmail.com>
2021-04-11 12:28:42 -0400

    disabled orgmode-to-html validate tag

--- init.el
+++ init.el
@@ -12,3 +12,17 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode ox-reveal zenburn-theme which-key use-package try smart-compile skewer-mode processing-mode orgalist org-multiple-keymap org-bullets multiple-cursors impatient-mode exec-path-from-shell elpy elfeed-org csharp-mode counsel auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
